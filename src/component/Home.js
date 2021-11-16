d4e0eb5b0f3be3042343f28e0a498c3448ec9c4e
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2021-11-16 14:17:16 +0900

    try camera

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
+import styled from "styled-components";
 
 function Home() {
+    var camera = document.getElementById('cameraFileInput');
+    var frame = document.getElementById('pictureFromCamera');
 
-    addEventListener("change", function() {
-        this.document
-        .getElementById("pictureFromCamera")
-        .setAttribute("src", window.URL.createObjectURL(this.files[0]));
+    addEventListener('change', function(e) {
+        var file = e.target.files[0];
+        document.src = URL.createObjectURL(file);
     });
 
     return(
         <div>
-            <input type="file" id="take-picture" accept="image/" capture="environment"/>
+            <input type="file" id="cameraFileInput" accept="image/*" capture="environment"/>
             <img id="pictureFromCamera" />
         </div>
     );
+    
+
 }
 
 export default Home;
