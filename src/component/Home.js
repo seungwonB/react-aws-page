b66f49f4c736dbe4b15ed67a8bc92c5f2c57977b
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2021-11-16 15:15:10 +0900

    fix: 카메라 사진 호출

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,23 +2,49 @@ import React, {useEffect} from "react";
 import styled from "styled-components";
 
 function Home() {
-    var camera = document.getElementById('cameraFileInput');
-    var frame = document.getElementById('pictureFromCamera');
+    useEffect(() => {
+        const camera = document.getElementById('cameraFileInput'); 
+        const frame = document.getElementById('pictureFromCamera');
 
-    addEventListener('change', function(e) {
-        var file = e.target.files[0];
-        document.src = URL.createObjectURL(file);
-    });
+        camera.addEventListener('change', function(e) {
+            var file = e.target.files[0];
+            frame.src = URL.createObjectURL(file);
+        });
+    }, []);
 
     return(
         <div>
-            <input type="file" id="cameraFileInput" accept="image/*" capture="environment"/>
-            <img id="pictureFromCamera" />
+            <label htmlFor="cameraFileInput">
+                <Btn_Camera>Open Camera</Btn_Camera>
+                <CameraInput type="file" id="cameraFileInput" accept="image/*" capture="environment"/>
+                <PicturCamaera id="pictureFromCamera" />
+            </label>
             <br></br>version1.0
         </div>
     );
     
 
 }
+
+const CameraInput = styled.input`
+    display: none;
+`;
+
+const PicturCamaera = styled.img`
+    width: 300px;
+    margin-top 16px;
+`;
+
+const Btn_Camera = styled.span`
+    display: inline-block;
+    background-color: #00b531;
+    color: white;
+    padding: 8px 12px;
+    border-radius: 4px;
+    font-size: 16px;   
+    cursor: pointer;
+`;
+
+
 
 export default Home;
